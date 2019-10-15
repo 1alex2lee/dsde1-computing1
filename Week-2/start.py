082e19b8700f9d1bbe48b2c8f5e6608433fa12cd
1alex2lee <lal19@ic.ac.uk>
2019-10-15 15:47:51 +0100

    Update start.py

--- Week-2/start.py
+++ Week-2/start.py
@@ -1,4 +1,3 @@
-'''
 name=input("What is your name?")
 print("Hello " + str(name))
 
@@ -8,23 +7,6 @@ print(math.pi)
 radius=5
 area=5*2*math.pi
 print(area)
-
-
-import random
-while True:
-    num=random.randint(1,5)
-    ans=int(input("Enter a number between 1 and 5: "))
-    state=ans==num
-    print(state)
-
-    while ans>5:
-            ans=int(input("Sorry, your number is not between 1 and 5. Please try again: "))
-    else:
-        if ans == num:
-            print("Congratulations. You won.")
-        else:
-            print("Sorry you lost.")
-
 
 def Function(args):
     print('this function is running')
@@ -37,12 +19,4 @@ while True:
     num1=input('Enter first number: ')
     num2=input('Enter second number: ')
     status=(num1==num2)
-    print(status)()
-
-
-
-while True:
-    num=num+1
-    print(num)
-
-'''
+    print(status)
