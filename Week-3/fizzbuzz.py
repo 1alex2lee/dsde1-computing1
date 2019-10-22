9b288230e373aeb961aee5b7ed017782b209d523
1alex2lee <lal19@ic.ac.uk>
2019-10-22 11:27:11 +0100

    Optimised fizzbuzz

--- Week-3/fizzbuzz.py
+++ Week-3/fizzbuzz.py
@@ -1,13 +1,13 @@
 num=1
 while num<101:
-    if (num/3).is_integer():
-        if (num/5).is_integer():
+    if (num%3==0):
+        if (num%5==0):
             print('FizzBuzz')
             num=num+1
         else:
             print('Fizz')
             num=num+1
-    elif (num/5).is_integer():
+    elif (num%5==0):
         print('Buzz')
         num=num+1
     else:
