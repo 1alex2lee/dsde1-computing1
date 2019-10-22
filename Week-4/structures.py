497658f55af84fa353ed0dc420abed9a9a20bea8
1alex2lee <lal19@ic.ac.uk>
2019-10-23 00:44:52 +0100

    Completed structures exercise

--- Week-4/structures.py
+++ Week-4/structures.py
@@ -9,7 +9,7 @@ Simple functions performing operations on basic Python data structures.
 # write a function that returns a list containig the first and the last element
 # of "the_list". 
 def first_and_last(the_list):
-    return (the_list[0], the_list[-1])
+    return [the_list[0], the_list[-1]]
 '''print (first_and_last([0,1,2,3,4,5,6]))'''
 
 
@@ -20,7 +20,7 @@ def first_and_last(the_list):
 # list, raise a "ValueError" exception. 
 def part_reverse(the_list, beginning, end):
     try:
-        new_list=the_list[end:beginning+1]
+        new_list=the_list[beginning:end]
         new_list.reverse()
     except:
         raise ValueError
@@ -32,7 +32,8 @@ print (part_reverse([0,1,2,3,4,5,6],5,2))'''
 # same value. For example if the_list = [0,1,2,3,4] and index = 3 the function
 # will return [0,1,2,3,3,3,4]. 
 def repeat_at_index(the_list, index):
-    the_list.insert(index, [the_list[index]]*2)
+    the_list.insert(index,the_list[index])
+    the_list.insert(index,the_list[index])
     return the_list
 '''
 print (repeat_at_index([0,1,2,3,4,5,6],4))'''
@@ -42,11 +43,12 @@ print (repeat_at_index([0,1,2,3,4,5,6],4))'''
 # write a function that checks whether the word is a palindrome, i.e. it reads
 # the same forward and backwards
 def palindrome_word(word):
+    word=word.lower()
     word=list(word)
     status = word==word[::-1]
     return status
-'''
-print (palindrome_word('racecar'))'''
+
+'''print (palindrome_word('Madam'))'''
 
 # write a function that checks whether the sentence is a palindrome, i.e. it
 # read the same forward and backward. Ignore all spaces and other characters
@@ -60,7 +62,7 @@ def palindrome_sentence(sentence):
     sentence=list(sentence)
     status=sentence==sentence[::-1]
     return status
-'''print (palindrome_sentence('Race, car.'))'''
+'''print (palindrome_word("Was it a car or a cat I saw"))'''
 
 # write a function that concatenates two sentences. First the function checks
 # whether the sentence meets the following criteria: it starts with a capital
@@ -68,16 +70,20 @@ def palindrome_sentence(sentence):
 # Keep in mind, that the sentence might have whitespace at the beginning or at
 # the end.  The concatenated sentence must have no white space at the beginning
 # or at the end and the must be exactly one space after the end of the first
-# sentence. 
+# sentence.
 def concatenate_sentences(sentence1, sentence2):
     import string
     sentence1=sentence1.strip()
     sentence2=sentence2.strip()
     status = sentence1[0].isupper()
     status = sentence2[0].isupper()
-    status = sentence1[-1].
-    return status
-print(concatenate_sentences('Ab','Ab'))
+    status = sentence1[-1] in string.punctuation
+    status = sentence2[-1] in string.punctuation
+    while status:
+        full = ''.join(sentence1)+' '+''.join(sentence2)
+        return full
+        break
+'''print(concatenate_sentences("First sentence.", "Second sentence."))'''
 
 
 # Dictionaries
@@ -85,14 +91,24 @@ print(concatenate_sentences('Ab','Ab'))
 # write a function that checks whether there is a record with given key in the
 # dictionary. Return True or False.
 def index_exists(dictionary, key):
-    return
+    if key in dictionary:
+        status = True
+    else:
+        status = False
+    return status
 
 # write a function which checks whether given value is stored in the
 # dictionary. Return True or False.
 def value_exists(dictionary, value):
-    return
+    if value in dictionary.values():
+        status = True
+    else:
+        status = False
+    return status
 
 # write a function that returns a new dictionary which contains all the values
 # from dictionary1 and dictionary2.
 def merge_dictionaries(dictionary1, dictionary2):
-    return
+    newdict = dictionary1.copy()
+    newdict.update(dictionary2)
+    return newdict
